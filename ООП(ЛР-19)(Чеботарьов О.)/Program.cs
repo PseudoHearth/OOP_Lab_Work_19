4df9b7288efce93f49baa209add54206a51cd594
админ <@DESKTOP-IMGPV8N>
2023-04-13 22:43:15 +0300

    Добавьте файлы проекта.

---
+++ ООП(ЛР-19)(Чеботарьов О.)/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("Enter any combination of numbers and spaces: ");
+        string input = Console.ReadLine();
+        string pattern = @"(?<!^\+?\d*)\D";
+
+        Console.Write("Combination without spaces exept first one: ");
+        string result = Regex.Replace(input, pattern, string.Empty);
+        Console.WriteLine(result);
+    }
+}
